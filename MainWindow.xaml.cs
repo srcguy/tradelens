0f8b39ba40c3d606c2a2bcdc3d01287d9a35395a
Bruno Szewczyk <81621691+srcguy@users.noreply.github.com>
2025-02-28 21:15:35 +0100

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -72,32 +72,19 @@ namespace tradelens
             plotModel.Axes.Add(X);
             plotModel.Axes.Add(Y);
 
-            List<string> urls = [ //lista adresow url plikow z cenami do pobrania
-            "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/" + currency + ".json"];
-
-            for (int i = 1; i<7; i++) //dodaje tyle plikow ile chce zeby wykres pokazywal (6 plikow tydzien, 30 miesiac itd)
-            {
-                urls.Add("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@" + DateTime.Now.AddDays(-i).ToString("yyyy-MM-dd") + "/v1/currencies/" + currency + ".json");
-            }
+            string[] files = Directory.GetFiles("data/" + currency);
 
             int n = 0;
 
-            foreach (string Url in urls)
+            foreach ( string file in files) 
             {
-                using (WebClient webClient = new WebClient()) //pobieram plik z neta
-                {
-                    webClient.DownloadFile(Url, "data/chart.json");
-                }
-
-                string jsonContent = File.ReadAllText("data/chart.json");
-
+                string jsonContent = File.ReadAllText(file);
                 using (JsonDocument doc = JsonDocument.Parse(jsonContent))//zamieniam tekst na jsondocument
                 {
                     double y = Math.Round(doc.RootElement.GetProperty(currency).GetProperty(File.ReadAllText("data/currency.txt")).GetDouble(), 4); //y ustawiam na odczytana wartosc pln z listy currency z dokladnoscia 4 miejsc p.p
 
                     lineSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Now.Date.AddDays(-n)), y)); //dodaje punkt o wartosci x jako date dzisiaj -iteracje i wartoscia y jako cene w tym dniu
                 }
-
                 n++;
             }
 
@@ -105,29 +92,30 @@ namespace tradelens
 
             wykres.Model = plotModel; //rysuje wykres na istniejacym obiekcie
         }
-        static double pln(string path, string currency)
+        static double convert(string path, string currency)
         {
             string jsonContent = File.ReadAllText(path); //czyta plik path
 
             using (JsonDocument doc = JsonDocument.Parse(jsonContent)) //zamienia tekst na jsondocument
             {
-                return doc.RootElement.GetProperty(currency).GetProperty(File.ReadAllText("data/currency.txt")).GetDouble(); //zwraca wartosc pln z listy currency (np usd) i zamienia na dobule
+                return doc.RootElement.GetProperty(currency).GetProperty(File.ReadAllText("data/currency.txt")).GetDouble(); //zwraca wartosc waluty z listy currency (np usd) i zamienia na dobule
             }
         }
 
         private void show_settings(object sender, RoutedEventArgs e)
         {
             var settingsPanel = new settingsPanel();
-            settingsPanel.usun += usunUstawienia; //dodaje zdarzenie
+            settingsPanel.usun += hide_settings; //dodaje zdarzenie
             settings.Content = settingsPanel;
         }
 
-        private void usunUstawienia()
+        private void hide_settings()
         {
             settings.Content = null; //usuwam settingspanel
             currenciesButtons();
             custom1Button();
             custom2Button();
+            downloadcustom();
         }
 
         private void currenciesButtons()
@@ -141,8 +129,8 @@ namespace tradelens
                 string url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/" + currency + ".json"; //najnowszy plik z cenami
                 string url_old = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@" + date.ToString("yyyy-MM-dd") + "/v1/currencies/" + currency + ".json"; //wczorajszy plik
 
-                string path = "data/curr.json";
-                string path_old = "data/curr_old.json";
+                string path = "data/curr_" + currency + ".json";
+                string path_old = "data/curr_old_" + currency + ".json";
 
                 using (WebClient webClient = new WebClient()) //pobieram pliki
                 {
@@ -150,9 +138,9 @@ namespace tradelens
                     webClient.DownloadFile(url_old, path_old);
                 }
 
-                double pln1 = pln(path, currency); //do pln1 przypisuje wynik funkcji pln z atrybutami path i currency
+                double pln1 = convert(path, currency); //do pln1 przypisuje wynik funkcji pln z atrybutami path i currency
 
-                double pln2 = pln(path_old, currency); //-||-
+                double pln2 = convert(path_old, currency); //-||-
 
                 Label label = FindName(currency) as Label; //zapisuje element o nazwie 'currency' jako label
                 Button label2 = FindName(currency + "_button") as Button; //zapisuje element o nazwie currency_button jako label2 (button)
@@ -185,8 +173,8 @@ namespace tradelens
             string url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/" + customcurrency1 + ".json"; //najnowszy plik z cenami
             string url_old = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@" + date.ToString("yyyy-MM-dd") + "/v1/currencies/" + customcurrency1 + ".json"; //wczorajszy plik
 
-            string path = "data/curr.json";
-            string path_old = "data/curr_old.json";
+            string path = "data/curr_" + customcurrency1 + ".json";
+            string path_old = "data/curr_old_" + customcurrency1 + ".json";
 
             using (WebClient webClient = new WebClient()) //pobieram pliki
             {
@@ -194,9 +182,9 @@ namespace tradelens
                 webClient.DownloadFile(url_old, path_old);
             }
 
-            double pln1 = pln(path, customcurrency1); //do pln1 przypisuje wynik funkcji pln z atrybutami path i currency
+            double pln1 = convert(path, customcurrency1); //do pln1 przypisuje wynik funkcji pln z atrybutami path i currency
 
-            double pln2 = pln(path_old, customcurrency1); //-||-
+            double pln2 = convert(path_old, customcurrency1); //-||-
 
             custom1.Content = Math.Round(pln1, 2); //zaokrąglenie do 2 miejsc p.p
 
@@ -226,8 +214,8 @@ namespace tradelens
             string url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/" + customcurrency1 + ".json"; //najnowszy plik z cenami
             string url_old = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@" + date.ToString("yyyy-MM-dd") + "/v1/currencies/" + customcurrency1 + ".json"; //wczorajszy plik
 
-            string path = "data/curr.json";
-            string path_old = "data/curr_old.json";
+            string path = "data/curr_" + customcurrency1 + ".json";
+            string path_old = "data/curr_old_" + customcurrency1 + ".json";
 
             using (WebClient webClient = new WebClient()) //pobieram pliki
             {
@@ -235,9 +223,9 @@ namespace tradelens
                 webClient.DownloadFile(url_old, path_old);
             }
 
-            double pln1 = pln(path, customcurrency1); //do pln1 przypisuje wynik funkcji pln z atrybutami path i currency
+            double pln1 = convert(path, customcurrency1); //do pln1 przypisuje wynik funkcji pln z atrybutami path i currency
 
-            double pln2 = pln(path_old, customcurrency1); //-||-
+            double pln2 = convert(path_old, customcurrency1); //-||-
 
             custom2.Content = Math.Round(pln1, 2); //zaokrąglenie do 2 miejsc p.p
 
@@ -258,15 +246,90 @@ namespace tradelens
             }
         }
 
+        private void downloadall()
+        {
+            string customcurrency1 = File.ReadAllText("data/custom1.txt");
+            string customcurrency2 = File.ReadAllText("data/custom2.txt");
+            List<string> currencies = ["usd", "eur", "gbp", "jpy", "chf", "cad", "btc", "eth", customcurrency1, customcurrency2]; //lista wszystkich walut
+            foreach (string currency in currencies)
+            {
+                List<string> urls = [ //lista adresow url plikow z cenami do pobrania
+                    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/" + currency + ".json"];
+
+                for (int i = 1; i < 7; i++) //dodaje tyle plikow ile chce zeby wykres pokazywal (6 plikow tydzien, 30 miesiac itd)
+                {
+                    urls.Add("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@" + DateTime.Now.AddDays(-i).ToString("yyyy-MM-dd") + "/v1/currencies/" + currency + ".json");
+                }
+
+                int n = 0;
+
+                foreach (string url in urls)
+                { 
+                    using (WebClient webClient = new WebClient()) //pobieram plik z neta
+                    {
+                        if (!Directory.Exists("data/" + currency + "/chart" + n + ".json"))
+                        {
+                            Directory.CreateDirectory("data/" + currency);
+                        }
+                        webClient.DownloadFile(url, "data/" + currency + "/chart" + n + ".json");
+                    }
+
+                    n++;
+                }
+            }
+        }
+
+        private async Task downloadasync()
+        {
+            download_text.Content = "Pobieranie danych...";
+            await Task.Run(() => downloadall());
+            download_text.Content = "";
+        }
+
+        private void downloadcustom()
+        {
+            string customcurrency1 = File.ReadAllText("data/custom1.txt");
+            string customcurrency2 = File.ReadAllText("data/custom2.txt");
+            List<string> currencies = [customcurrency1, customcurrency2]; //lista wszystkich walut
+            foreach (string currency in currencies)
+            {
+                List<string> urls = [ //lista adresow url plikow z cenami do pobrania
+                    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/" + currency + ".json"];
+
+                for (int i = 1; i < 7; i++) //dodaje tyle plikow ile chce zeby wykres pokazywal (6 plikow tydzien, 30 miesiac itd)
+                {
+                    urls.Add("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@" + DateTime.Now.AddDays(-i).ToString("yyyy-MM-dd") + "/v1/currencies/" + currency + ".json");
+                }
+
+                int n = 0;
+
+                foreach (string url in urls)
+                {
+                    using (WebClient webClient = new WebClient()) //pobieram plik z neta
+                    {
+                        if (!Directory.Exists("data/" + currency + "/chart" + n + ".json"))
+                        {
+                            Directory.CreateDirectory("data/" + currency);
+                        }
+                        webClient.DownloadFile(url, "data/" + currency + "/chart" + n + ".json");
+                    }
+
+                    n++;
+                }
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            _ = downloadasync();
 
             currenciesButtons();
 
             custom1Button();
 
             custom2Button();
+
         }
 
     }
